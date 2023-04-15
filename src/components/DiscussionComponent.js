2ef3808f78c02f9908d4284e47dcd38c18fb7afa
kzheng0915 <katieqiaozheng@gmail.com>
2023-04-15 18:36:33 -0400

    Adjusted discussion screen

--- src/components/DiscussionComponent.js
+++ src/components/DiscussionComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList, Image, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList, Image, KeyboardAvoidingView, Platform, Keyboard } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Icons from '../constants/Icons';
 //import { FlatList } from 'react-native-gesture-handler';
@@ -28,10 +28,22 @@ const posts = [
     //image: Icons.reading,
     timestamp: '6:30 2/18/23',
   },
+  {
+    id: 4,
+    author: 'David',
+    avatar: Icons.avatar,
+    content: 'This is another sample text post.',
+    //image: Icons.reading,
+    timestamp: '18:20 7/11/22',
+  },
 ];
 
 function DiscussionComponent() {
   const [message, setMessage] = useState('');
+
+  function handleSend(){
+
+  }
 
   function renderItem({item}) {
     return (
@@ -59,27 +71,31 @@ function DiscussionComponent() {
     style={{ flex: 1 }}
     behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.container}>
-                <FlatList data={posts}
-                        renderItem={renderItem}
-                        keyExtractor={(item) => item.id.toString()}
-                        contentContainerStyle={styles.scrollView}>
-                        {/* style={styles.scrollView}> */}
-                </FlatList>
+        <View style={styles.container}>
+            <FlatList data={posts}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id.toString()}
+                    contentContainerStyle={styles.scrollView}>
+            </FlatList>
                 <View style={styles.inputContainer}>
                     <TextInput
                     style={styles.input}
                     placeholder="Write your message..."
                     onChangeText={setMessage}
                     value={message}
+                    // returnKeyType="send"
+                    // onSubmitEditing={handleSend}
                     />
-                    <TouchableOpacity style={styles.sendButton}>
-                    <Ionicons name="send" size={30} color="orange" />
-                    </TouchableOpacity>
+                    <View style={styles.iconContainer}>
+                        <TouchableOpacity style={styles.addButton}>
+                                <Ionicons name="camera-sharp" size={30} color="orange" />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.sendButton}>
+                            <Ionicons name="send" size={25} color="orange" />
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
-        </TouchableWithoutFeedback>
+        </View>
     </KeyboardAvoidingView>
   );
 }
@@ -87,18 +103,19 @@ function DiscussionComponent() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      //backgroundColor: '#fff',
       backgroundColor: '#f0f0f0',
     },
     scrollView: {
-      padding: 10,
+      //padding: 10,
+    },
+    flatListContainer: {
+        flex: 1
     },
     post: {
       flexDirection: 'row',
       alignItems: 'flex-start',
       marginTop: 10,
       marginBottom: 20,
-
     },
     avatar: {
       width: 50,
@@ -123,7 +140,7 @@ const styles = StyleSheet.create({
     // },
     postBackground: {
         backgroundColor: '#fff',
-        borderRadius: 10,
+        //borderRadius: 10,
         padding: 10,
         marginBottom: 10,
       },
@@ -145,27 +162,51 @@ const styles = StyleSheet.create({
       //marginVertical: 10,
     },
     inputContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 10,
-      borderTopWidth: 1,
-      borderColor: '#eee',
-      height: 100,
-      backgroundColor: '#fff',
-    },
-    input: {
-      flex: 1,
-      borderColor: '#ccc',
-      borderRadius: 20,
-      paddingHorizontal: 15,
-      //paddingVertical: 5,
-      marginRight: 10,
-    },
-    sendButton: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: 50
-    }
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        padding: 10,
+        borderTopWidth: 1,
+        borderColor: '#eee',
+        backgroundColor: '#fff',
+        height: 100
+      },
+      input: {
+        flex: 1,
+        marginTop: 10,
+        marginLeft: 5,
+        backgroundColor: '#f8f8f8', // Add a background color
+        borderColor: '#ccc',
+        borderWidth: 1, // Add a border
+        borderRadius: 25, // Increase the border radius
+        paddingHorizontal: 20, // Increase the horizontal padding
+        paddingVertical: 10, // Add vertical padding
+        marginRight: 10,
+        fontSize: 16, // Increase the font size
+      },
+      sendContainer:{
+        flexDirection: 'row',
+        alignItems: 'center'
+      },    
+      addButton: {
+        marginTop: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 15,
+        //width: 50
+      },
+      sendButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        //width: 50,
+        marginTop: 15,
+        marginRight: 5,
+      },
+      iconContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
 });
    
 export default DiscussionComponent;
